f9bd2cd9480a7bd6b0f31ad2702cd100d161bc43
Joel Haker <joeladam@gmail.com>
2018-11-12 03:35:31 -0500

    Reconfigured config

--- site/mqttflask.py
+++ site/mqttflask.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, cli as flask_cli
 from flask_restful import Api
-from instance import config as instance_config
+from config import config as application_config
 from controllers.home import HomeController
 from controllers.mqtt import MqttController
 
@@ -10,12 +10,7 @@ dotenv_path = os.path.join(app_root, '.env')
 flask_cli.load_dotenv(dotenv_path)
 
 app = Flask(__name__, static_folder='assets', template_folder="resources/templates", root_path=app_root)
-
-if os.getenv('FLASK_ENV') == 'development':
-    app.config.from_object(instance_config.DevelopmentConfig)
-else:
-    app.config.from_object(instance_config.ProductionConfig)
-
+app.config.from_object(application_config)
 api = Api(app)
 
 # Regular route that renders a web page
@@ -25,5 +20,4 @@ app.add_url_rule('/', view_func=HomeController.as_view('home_page'))
 api.add_resource(MqttController, '/mqtt', '/mqtt/<string:topic>')
 
 if __name__ == "__main__":
-    #app.run(host='0.0.0.0', port=80, debug=True)
     app.run()

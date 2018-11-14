3253f09e5f1122ac658c078a22120d60e0c3d931
Joel Haker <joeladam@gmail.com>
2018-11-13 20:53:05 -0500

    WIP Move Files

--- site/resources/js/bin/cleanup.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var path = require('path');
-
-var current_dir = path.resolve(__dirname);
-var site_dir    = path.resolve(current_dir, '../../../');
-var dist_dir    = path.resolve(site_dir, 'dist/');
-
-fs.rmdir(dist_dir, () => console.log('Done!'));
